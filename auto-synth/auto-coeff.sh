5ee4b853e2694c43854e53267bcee5cdc4ea81a5
DengQi <dengqi935@gmail.com>
2018-12-12 20:32:30 +0800

    fix typo

--- auto-synth/auto-coeff.sh
+++ auto-synth/auto-coeff.sh
@@ -9,6 +9,7 @@ else
     coeffients=`cat $2`
     echo $coeffients
     # coeff2code $coeffients > /tmp/ee252/${uuidx}.vhd
+    coeff2code $coeffients 
     # auto-synth /tmp/ee252/${uuidx}.vhd $1
     # rm -rf /tmp/ee252/${uuidx}.vhd
 fi
