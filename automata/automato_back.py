93a42c81c4380bcbd71920ac4d8b7be0e9ac51d2
hafumi <hafumi@shanghaitech.edu.cn>
2018-12-12 23:33:35 +0800

    upload && coefficients support

--- automata/automato_back.py
+++ automata/automato_back.py
@@ -1,7 +1,9 @@
+import os
 from flask import Flask, request, render_template, jsonify, send_from_directory, abort
 import subprocess as sb
 from uuid import uuid1
 from os import path
+import mistune
 
 
 _base_path = path.dirname(path.realpath(__file__))
@@ -10,7 +12,7 @@ app = Flask(__name__, static_url_path="/file")
 
 @app.route('/')
 def index():
-    return render_template('index.html')
+    return render_template('setting.html')
 
 
 @app.route('/synth', methods=["POST"])
@@ -28,12 +30,47 @@ def synth():
     with open(coefficients_file_path, "w") as file:
         file.write(coefficients)
     uuid_str2 = str(uuid1().hex)
-    sb.run(["bash", path.join(_base_path, "flask_synth.sh"), "coefficients_file_path", str(clock),
-            uuid_str2], shell=True, timeout=120)
+    sb.run(" ".join(["bash", path.join(_base_path, "flask_synth.sh"), coefficients_file_path, str(clock),
+            uuid_str2, ">output.txt"]), shell=True, timeout=120)
     return jsonify({
         "code": 200,
         "messgae": uuid_str2
     })
+
+@app.route('/synth2', methods=["POST"])
+def synth2():
+    try:
+        clock = request.form["clock"]
+        coefficients = request.form["coefficient"]
+    except:
+        return abort(500)
+    uuid_str = str(uuid1().hex)
+    coefficients_file_path = '/tmp/ee252/coefficients/' + uuid_str + ".txt"
+    with open(coefficients_file_path, "w") as file:
+        file.write(coefficients)
+    uuid_str2 = str(uuid1().hex)
+    sb.run(" ".join(["bash", path.join(_base_path, "flask_synth.sh"), coefficients_file_path, str(clock),
+            uuid_str2]), shell=True, timeout=120)
+    os.remove(coefficients_file_path)
+    return render_template('success.html', uuid_str=uuid_str2)
+
+@app.route('/synthfile', methods=["POST"])
+def synthfile():
+    try:
+    #if 1:
+        clock = request.form["clock"]
+        file = request.files['vhdl']
+    except:
+        return abort(500)
+    uuid_str = str(uuid1().hex)
+    update_path = "/tmp/ee252/" + uuid_str+".vhd"
+    print(update_path)
+    file.save(update_path)
+    uuid_str2 = str(uuid1().hex)
+    sb.run(" ".join(["bash", path.join(_base_path, "flask_synth_upload.sh"), update_path, str(clock),
+            uuid_str2, ">output.txt"]), shell=True, timeout=120)
+    os.remove(update_path) 
+    return render_template('success.html', uuid_str=uuid_str2)
 
 
 @app.route('/qor/<string:uuid_str>')
@@ -52,6 +89,22 @@ def qor(uuid_str):
             "code": 200,
             "message": qor_logs
         })
+
+@app.route('/view/<string:uuid_str>')
+def view(uuid_str):
+    qor_logs = ""
+    try:
+        with open(path.join(_base_path, "qor", uuid_str + ".txt"), 'r') as file:
+            qor_logs = file.read()
+        qor_logs = qor_logs.replace("\n", "\n\n")
+        qor_logs = mistune.markdown(qor_logs)
+    except:
+        return jsonify({
+            "code": 404,
+            "messgae": "results not found"
+        })
+    else:
+        return qor_logs
 
 
 @app.route('/zip/<string:uuid_str>')
--- automata/flask_synth_upload.sh
+++ automata/flask_synth_upload.sh
@@ -4,27 +4,17 @@ uuid1=`uuidgen`
 
 mkdir -p /tmp/ee252
 
-coeff=`cat $1`
-
-echo  $coeff | awk '{ for (i=1;i<=NF;i++)printf "%s\n",$i}' > /tmp/ee252/${uuid1}.txt
-
-uuid2=`uuidgen`
 uuid3=`uuidgen`
-
-echo $1
-
-mcm_synth -nofs -gc $coeff > /tmp/ee252/${uuid2}.txt
-
 path=`pwd`
 real_time=`date '+%Y%m%d-%H%-M-%S'`
 mkdir -p /tmp/ee252/$uuid3
 mkdir -p /tmp/ee252/$uuid3/report
 mkdir -p /tmp/ee252/$uuid3/output
 mkdir -p /tmp/ee252/$uuid3/rtl
-cp /var/ee252/setup.tcl /tmp/ee252/$uuid3/
+cp $1 /tmp/ee252/$uuid3/rtl/Top.vhd
 # mv  /tmp/ee252/${uuid4}.vhd /tmp/ee252/$uuid3/rtl/Top.vhd
-vhdl-gen /tmp/ee252/${uuid1}.txt /tmp/ee252/${uuid2}.txt > /tmp/ee252/$uuid3/rtl/Top.vhd
 cp /var/ee252/REG.vhd /tmp/ee252/$uuid3/rtl/
+cp /var/ee252/setup.tcl /tmp/ee252/$uuid3/
 cd /tmp/ee252/$uuid3
 sed -i "s/CLOCK_PERIOD/$2/g" setup.tcl > /dev/null
 dc_shell-t -f setup.tcl
@@ -36,4 +26,3 @@ mv "synth${3}.zip"  $path/zip/
 cd $path
 
 rm -rf /tmp/ee252/$uuid3
-rm /tmp/ee252/${uuid2}.txt /tmp/ee252/${uuid1}.txt
