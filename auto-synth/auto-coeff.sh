8e364dc6986cefe43bfc718e52e5d769cf2d97d4
DengQi <dengqi935@gmail.com>
2018-12-12 20:24:10 +0800

    debug

--- auto-synth/auto-coeff.sh
+++ auto-synth/auto-coeff.sh
@@ -2,11 +2,12 @@
 
 if [ -z $1 ] && [ -z $2 ]; then
     echo "Help:"
-    echo "auto-coeff [clock] [coeffients]"
+    echo "auto-coeff [clock] [coeffients_file_inline]"
     echo 'You need to use " to warp the coeffients up!'
 else
     uuidx=`uuidgen`
-    coeff2code $2 > /tmp/ee252/${uuidx}.vhd
+    coeffients=`cat $2`
+    coeff2code $coeffients > /tmp/ee252/${uuidx}.vhd
     auto-synth /tmp/ee252/${uuidx}.vhd $1
     rm -rf /tmp/ee252/${uuidx}.vhd
 fi
